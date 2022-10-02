b4bbf68d3014f066278d61d47c77e6b263deec52
riskGod06 <jopsan63@gmail.com>
2022-10-02 13:11:29 -0500

    update busquedaOrdenamiento

--- BusquedaOrdenamiento.cs
+++ BusquedaOrdenamiento.cs
@@ -43,6 +43,52 @@ namespace BusquedaOrdenamiento{
             // imp(L, n);
         }
 
+        public void min(int[] L, int n)
+        {
+            int[] win = {n};
+            int[] L2 = {n};
+            int index = 1;
+
+            for(int i = 1; i <= n; i++)
+            {
+                Parallel.For(0, i++, index => {
+                    win[i] = 0;
+                });
+            }
+
+            for(int i = 1; i <= n; i++)
+            {
+                Parallel.For(0, i++, index => {
+                    for(int j = 1; j <= n; j++)
+                    {
+                        if(L[i] > L[j])
+                        {
+                            win[i] = 1;
+                        }else{
+                            win[j] = 1;
+                        }
+                    }
+                });
+            }
+
+            for(int i = 1; i <= n; i++)
+            {
+                Parallel.For(0, i++, index => {
+                    if(win[i] == 0)
+                    {
+                        // L2[index] = L[i];
+                        index = 1;
+                    }
+                });
+
+            }
+
+            for(int i = 1; i <= n; i++)
+            {
+                Console.WriteLine(L[i]);
+            }
+        }
+
         public void imp (int[] L, int n)
         {
             for(int i = 0; i <= n; i++)
